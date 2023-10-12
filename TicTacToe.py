d67874e67d0b8b4a06d95f11c0b7d8d12497b400
Mrunmayee <mrunmayeej.2000@gmail.com>
2023-10-12 22:43:59 +0900

    placing marker logic of winning choosing player

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 from IPython.display import clear_output
+import random 
 def display_board(board) :
     clear_output()
     print(board[1], board[2], board[3])
@@ -6,7 +7,6 @@ def display_board(board) :
     print(board[7], board[8], board[9])
 
 test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
 
 def player_input():
     marker = ''
@@ -20,3 +20,24 @@ def player_input():
         player2 = 'X'
     return (player1, player2)
 
+def place_marker(board, marker, position):
+    board[position] = marker
+
+def win_check(board, mark):
+    return (
+        (board[1] == mark and board[2] == mark and board[3] == mark) or
+        (board[4] == mark and board[5] == mark and board[6] == mark) or
+        (board[7] == mark and board[8] == mark and board[9] == mark) or
+        (board[1] == mark and board[4] == mark and board[7] == mark) or
+        (board[2] == mark and board[5] == mark and board[8] == mark) or
+        (board[3] == mark and board[6] == mark and board[9] == mark) or
+        (board[1] == mark and board[5] == mark and board[9] == mark) or
+        (board[3] == mark and board[5] == mark and board[7] == mark) 
+    )
+
+def choose_first():
+    flip = random.randint(0,1)
+    if flip == 0:
+        return 'Player 1'
+    else: 
+        return 'Player 2'
