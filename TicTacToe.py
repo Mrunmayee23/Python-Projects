67923094818f60be8b67cf1725cd18449f68931a
Mrunmayee <mrunmayeej.2000@gmail.com>
2023-10-13 00:10:27 +0900

    the main logic

--- TicTacToe.py
+++ TicTacToe.py
@@ -61,3 +61,60 @@ def replay():
     choice = input('Want to play again? Type yes or no : ')
 
     return choice == 'yes'
+
+
+print('Welcome to Tic Tac Toe!')
+while True:
+    the_board = [' '] * 10
+
+    player1_marker, player2_marker = player_input()
+
+    turn = choose_first()
+    print(turn + ' will go first')
+
+    play_game = input('are you ready? y or n: ')
+
+    if play_game == 'y':
+        game_on = True
+    else: 
+        game_on = False
+
+    while game_on:
+        if turn == 'Player 1':
+            display_board(the_board)
+            position = player_choice(the_board)
+            place_marker(the_board, player1_marker, position)
+
+            if win_check(the_board, player1_marker):
+                display_board(the_board)
+                print('Player 1 has won!')
+                game_on = False
+
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('TIE')
+                    break
+                else:
+                    turn = 'Player 2'
+        
+        else:
+             display_board(the_board)
+             position = player_choice(the_board)
+             place_marker(the_board, player2_marker, position)
+
+             if win_check(the_board, player2_marker):
+                display_board(the_board)
+                print('Player 2 has won!')
+                game_on = False
+
+             else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('TIE')
+                    break
+                else:
+                    turn = 'Player 1'
+
+    if not replay():
+        break
