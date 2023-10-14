87679ea02d4ac4da3db07ceb666fdf77f3c554e2
Mrunmayee <mrunmayeej.2000@gmail.com>
2023-10-14 22:52:36 +0900

    deck class and hand class

--- BlackJack.py
+++ BlackJack.py
@@ -7,6 +7,7 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 
 playing = True
 
+#CARD CLASS
 class Card:
     
     def __init__(self, suit, rank):
@@ -15,3 +16,50 @@ class Card:
     
     def __str__(self):
         return self.rank + " of " + self.suit
+    
+#DECK CLASS
+class Deck:
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+    
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return "The deck has: " + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+    
+test_deck = Deck()
+print(test_deck)
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+        #card passed in is actually going to be from the Deck.deal()
+        self.cards.append(card)
+        self.value += values[card.rank]
+        
+        #track aces
+        if card.rank == 'Ace':
+            self.aces += 1
+    
+    def adjust_for_ace(self):
+        #If total value > 21 and I still have an ace
+        #Then change my ace to be 1 instead of 11
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
